4d518f8e0c71a952444c3ebcb9facbb48834f6f6
Rob Majuri <rmajuri@urbn.com>
2021-08-21 17:28:45 -0400

    Clean things up

--- newstrackerapi/serializers.py
+++ newstrackerapi/serializers.py
@@ -1,16 +1,8 @@
 from rest_framework import serializers
 from .models import Article
 
-# class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Article
-#         fields = ('id', 'name', 'link', 'description')
 
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-    # name = serializers.CharField(max_length=60)
-    # link = serializers.CharField(max_length=2000)
-    # description = serializers.CharField(max_length=250)
-
     class Meta:
         model = Article
         fields = ('id', 'name', 'link', 'description')
--- newstrackerapi/views.py
+++ newstrackerapi/views.py
@@ -1,19 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
-# from rest_framework import viewsets
-#
 from .serializers import ArticleSerializer
-from django.shortcuts import get_object_or_404
-# from .models import Article
-#
-# class ArticleViewSet(viewsets.ModelViewSet):
-#     queryset = Article.objects.all().order_by('name')
-#     serializer_class = ArticleSerializer
-
+from .models import Article
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Article
+from django.shortcuts import get_object_or_404
 
 
 class ArticleView(APIView):
@@ -49,3 +38,4 @@ class ArticleView(APIView):
         article = get_object_or_404(Article.objects.all(), pk=pk)
         article.delete()
         return Response(status=204)
+
