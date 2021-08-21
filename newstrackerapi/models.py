a4941a4931581f4374424f5112fa3441e6237439
Rob Majuri <rmajuri@urbn.com>
2021-08-21 17:28:45 -0400

    Clean things up

--- newstrackerapi/models.py
+++ newstrackerapi/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+
 class Article(models.Model):
     name = models.CharField(max_length=60, default='')
     link = models.CharField(max_length=2000)
@@ -9,5 +8,3 @@ class Article(models.Model):
 
     def _str_(self):
         return self.name
-
-
--- newstrackerapi/urls.py
+++ newstrackerapi/urls.py
@@ -1,14 +1,5 @@
 from django.urls import include, path
-from rest_framework import routers
-from .import views
-
-# router = routers.DefaultRouter()
-# router.register(r'articles', views.ArticleViewSet)
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
+from . import views
 
 urlpatterns = [
     path('articles/', views.ArticleView.as_view()),
