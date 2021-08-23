9da203cb277c973efcb7e2848d37446bda56fabe
Rob Majuri <rmajuri@urbn.com>
2021-08-22 23:49:19 -0400

    Env setup

--- newstracker/settings.py
+++ newstracker/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,11 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Necessary for developing when using a separate React project
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/'
-]
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
