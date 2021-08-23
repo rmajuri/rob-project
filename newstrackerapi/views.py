8e2f0f4f393f63d7c3eb5c0fe8bd57bcf3853fde
Rob Majuri <rmajuri@urbn.com>
2021-08-22 23:11:38 -0400

    Add production build of FE

--- newstrackerapi/views.py
+++ newstrackerapi/views.py
@@ -1,50 +1,58 @@
-import os
-from django.http import HttpResponse
-from .serializers import ArticleSerializer
 from .models import Article
+from .serializers import ArticleSerializer
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+import os
 import requests
 from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin, urlparse
-from django.views.generic import View
-from django.conf import settings
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s : %(message)s'
+)
+LOGGER = logging.getLogger('rob-project')
 
 
 class ArticleView(APIView):
 
     def _is_valid(self, url):
-        """
-        Checks whether `url` is a valid URL.
-        """
+
+        # Check for a valid URL
         parsed = urlparse(url)
         return bool(parsed.netloc) and bool(parsed.scheme)
 
     def _get_all_images(self, url):
-        """
-        Returns all image URLs on a single `url`
-        """
-        soup = bs(requests.get(url).content, "html.parser")
+
+        LOGGER.info('Getting URL Images')
+        # Return images for a valid URL
+        soup = bs(requests.get(url).content, 'html.parser')
 
         urls = []
-        for img in tqdm(soup.find_all("img"), "Extracting images"):
-            img_url = img.attrs.get("src")
+        for img in tqdm(soup.find_all('img'), 'Extracting images'):
+            img_url = img.attrs.get('src')
             if not img_url:
                 # if img does not contain src attribute, just skip
                 continue
             img_url = urljoin(url, img_url)
             try:
-                pos = img_url.index("?")
+                pos = img_url.index('?')
                 img_url = img_url[:pos]
-            except ValueError:
+            except ValueError as e:
+                LOGGER.exception(e)
                 pass
                 if self._is_valid(img_url):
                     urls.append(img_url)
             return urls
 
     def get(self, request):
+
+        LOGGER.info('Getting all articles')
 
         articles = Article.objects.all().order_by('name')
 
@@ -53,6 +61,9 @@ class ArticleView(APIView):
         return Response(serializer.data)
 
     def post(self, request):
+
+        LOGGER.info('Adding new article')
+
         article = request.data
         serializer = ArticleSerializer(data=article)
 
@@ -71,6 +82,9 @@ class ArticleView(APIView):
         return Response(serializer.data)
 
     def put(self, request, pk):
+
+        LOGGER.info('Updating article')
+
         saved_article = get_object_or_404(Article.objects.all(), pk=pk)
         data = request.data
         serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
@@ -81,6 +95,9 @@ class ArticleView(APIView):
         return Response(serializer.data)
 
     def delete(self, request, pk):
+
+        LOGGER.info('Deleting article')
+
         article = get_object_or_404(Article.objects.all(), pk=pk)
         article.delete()
         return Response(status=204)
@@ -88,8 +105,12 @@ class ArticleView(APIView):
 
 class FrontendAppView(View):
     def get(self, request):
+
+        LOGGER.info('Serving frontend')
+
         try:
             with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
                 return HttpResponse(f.read())
-        except FileNotFoundError:
+        except FileNotFoundError as e:
+            LOGGER.exception(e)
             return HttpResponse(status=501)
